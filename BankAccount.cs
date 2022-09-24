cc9baaf62e266eecb47a0c6da4370238863c67d3
ahmad <bha296780@gmail.com>
2022-09-24 14:34:50 -0400

    final

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Encapsulation
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public void  deposit(double balance)
+        {
+            this.balance += balance;
+        }
+        public void withdraw(double balance)
+        { this.balance -= balance; }
+        public void getBalance()
+        {
+            Console.WriteLine(balance); 
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using Bank_Encapsulation;
+
+BankAccount chase = new BankAccount();
+chase.deposit(22);
+chase.getBalance();
+Console.WriteLine("------");
+chase.withdraw(15);
+chase.getBalance();
+Console.WriteLine("-----");
+
+Console.WriteLine("What would you like to do with your bank account! Deposit or Withdraw ?");
+string user = Console.ReadLine();
+if (user == "deposit")
+{
+    Console.WriteLine("how much");
+    int thismuch = int.Parse(Console.ReadLine());
+    chase.deposit(thismuch);
+    Console.WriteLine("your new balance is:");
+    chase.getBalance();
+}
+else if (user == "Deposit")
+{
+    Console.WriteLine("how much");
+    int thismuch = int.Parse(Console.ReadLine());
+    chase.deposit(thismuch);
+    Console.WriteLine("your new balance is:");
+    chase.getBalance();
+}
+else if (user == "withdraw")
+{
+    Console.WriteLine("how much");
+    int thismuch = int.Parse(Console.ReadLine());
+    chase.withdraw(thismuch);
+    Console.WriteLine("your new balance is:");
+    chase.getBalance();
+}
+else if (user == "Withdraw")
+{
+    Console.WriteLine("how much");
+    int thismuch = int.Parse(Console.ReadLine());
+    chase.withdraw(thismuch);
+    Console.WriteLine("your new balance is:");
+    chase.getBalance();
+
+}
+else
+{
+    Console.WriteLine("CHOOSE ONE OF THE ABOVE");
+}
+
